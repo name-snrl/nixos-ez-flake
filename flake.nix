57e514662157a30cc9cb245f924663aaa068e7c0
name_snrl <Demogorgon-74@ya.ru>
2024-03-30 07:23:58 +0500

    init flake with lib

---
+++ flake.nix
@@ -0,0 +1,62 @@
+{
+  description = "Filesystem-based NixOS configuration";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    with nixpkgs.lib;
+    rec {
+      mkModuleTree =
+        dir:
+        mapAttrs'
+          (
+            name: type:
+            if type == "directory" then
+              nameValuePair name (mkModuleTree /${dir}/${name})
+            else if name == "default.nix" then
+              nameValuePair "self" /${dir}/${name}
+            else
+              nameValuePair (removeSuffix ".nix" name) /${dir}/${name}
+          )
+          (filterAttrs (name: type: type == "directory" || hasSuffix ".nix" name) (builtins.readDir dir));
+
+      mkHosts =
+        dir: inputs:
+        let
+          importsFromAttrs =
+            {
+              importByDefault,
+              modules,
+              imports,
+            }:
+            let
+              modulesToList = xs: flatten (mapAttrsToList (_: v: if isPath v then v else modulesToList v) xs);
+              convertedImports =
+                mapAttrsRecursive
+                  (
+                    path: value:
+                    throwIfNot (isBool value && hasAttrByPath path modules)
+                      "Check the path ${concatStringsSep "." path}, the value should be of type boolean and exist in modules"
+                      (if value then getAttrFromPath path modules else { })
+                  )
+                  imports;
+            in
+            modulesToList (
+              if importByDefault then recursiveUpdate modules convertedImports else convertedImports
+            );
+        in
+        genAttrs (attrNames (filterAttrs (_: type: type == "directory") (builtins.readDir dir))) (
+          name:
+          nixosSystem {
+            specialArgs = {
+              inherit inputs importsFromAttrs;
+            };
+            modules = [
+              /${dir}/${name}
+              { networking.hostName = name; }
+            ];
+          }
+        );
+    };
+}
