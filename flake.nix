b4d4dde59a5d5adbc29c2c98f35485d73a3ef46a
name_snrl <Demogorgon-74@ya.ru>
2024-03-30 10:53:19 +0500

    lib: add global import to mkHosts

--- flake.nix
+++ flake.nix
@@ -9,20 +9,22 @@
     rec {
       mkModuleTree =
         dir:
-        mapAttrs'
-          (
-            name: type:
-            if type == "directory" then
-              nameValuePair name (mkModuleTree /${dir}/${name})
-            else if name == "default.nix" then
-              nameValuePair "self" /${dir}/${name}
-            else
-              nameValuePair (removeSuffix ".nix" name) /${dir}/${name}
-          )
-          (filterAttrs (name: type: type == "directory" || hasSuffix ".nix" name) (builtins.readDir dir));
+        mapAttrs' (
+          name: type:
+          if type == "directory" then
+            nameValuePair name (mkModuleTree /${dir}/${name})
+          else if name == "default.nix" then
+            nameValuePair "self" /${dir}/${name}
+          else
+            nameValuePair (removeSuffix ".nix" name) /${dir}/${name}
+        ) (filterAttrs (name: type: type == "directory" || hasSuffix ".nix" name) (builtins.readDir dir));
 
       mkHosts =
-        dir: inputs:
+        {
+          entryPoint,
+          inputs,
+          globalImports ? [ ],
+        }:
         let
           importsFromAttrs =
             {
@@ -32,30 +34,27 @@
             }:
             let
               modulesToList = xs: flatten (mapAttrsToList (_: v: if isPath v then v else modulesToList v) xs);
-              convertedImports =
-                mapAttrsRecursive
-                  (
-                    path: value:
-                    throwIfNot (isBool value && hasAttrByPath path modules)
-                      "Check the path ${concatStringsSep "." path}, the value should be of type boolean and exist in modules"
-                      (if value then getAttrFromPath path modules else { })
-                  )
-                  imports;
+              convertedImports = mapAttrsRecursive (
+                path: value:
+                throwIfNot (isBool value && hasAttrByPath path modules)
+                  "Check the path ${concatStringsSep "." path}, the value should be of type boolean and exist in modules"
+                  (if value then getAttrFromPath path modules else { })
+              ) imports;
             in
             modulesToList (
               if importByDefault then recursiveUpdate modules convertedImports else convertedImports
             );
         in
-        genAttrs (attrNames (filterAttrs (_: type: type == "directory") (builtins.readDir dir))) (
+        genAttrs (attrNames (filterAttrs (_: type: type == "directory") (builtins.readDir entryPoint))) (
           name:
           nixosSystem {
             specialArgs = {
               inherit inputs importsFromAttrs;
             };
             modules = [
-              /${dir}/${name}
+              /${entryPoint}/${name}
               { networking.hostName = name; }
-            ];
+            ] ++ globalImports;
           }
         );
     };
