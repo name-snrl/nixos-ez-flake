0915e44c47feb79b1bafd761305b9e09cd4398a2
name_snrl <72071763+name-snrl@users.noreply.github.com>
2024-10-25 19:43:37 +0500

    templates: fix `nupdate` alias

--- templates/nixos-configuration/modules/nixos/profiles/aliases.nix
+++ templates/nixos-configuration/modules/nixos/profiles/aliases.nix
@@ -7,7 +7,7 @@ in
     jnp = "cd ${config.nixpkgs.flake.source}";
     nboot = "nixos-rebuild boot --use-remote-sudo --flake ${cfgPath}";
     nswitch = "nixos-rebuild switch --use-remote-sudo --flake ${cfgPath}";
-    nupdate = "nix flake update --commit-lock-file ${cfgPath}";
+    nupdate = "nix flake update --commit-lock-file --flake ${cfgPath}";
     # https://github.com/NixOS/nix/issues/8508
     nclear = "sudo nix-collect-garbage --delete-old && nix-collect-garbage --delete-old";
   };
